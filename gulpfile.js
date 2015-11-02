36c7fb1a28435216ae548deb9365b3164cf7e102
Tabris841 <shadowtabris84@gmail.com>
2015-11-02 19:39:33 +0200

    add react-router

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,8 @@ gulp.task('js', function() {
 gulp.task('css', function() {
 	gulp.src(config.paths.css)
 		.pipe(concat('bundle.css'))
-		.pipe(gulp.dest(config.paths.dist + '/css'));
+		.pipe(gulp.dest(config.paths.dist + '/css'))
+		.pipe(connect.reload());
 });
 
 // Migrates images to dist folder
--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,10 +3,26 @@
 var React = require('react');
 
 var About = React.createClass({
-    render: function() {
+    statics: {
+        willTransitionTo: function (transition, params, quety, callback) {
+            if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+                transition.about();
+            } else {
+                callback();
+            }
+        },
+
+        willTransitionFrom: function (transition, compoment) {
+            if (!confirm('Are you sure you want to leave a page that\'s that exciting?')) {
+                transition.about();
+            }
+        }
+    },
+    render: function () {
         return (
             <div>
                 <h1>About</h1>
+
                 <p>
                     This application uses the following technologies:
                     <ul>
